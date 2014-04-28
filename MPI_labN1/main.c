530e98bebdcbfe2c7d4ade3d43906a9c1ceacfbc
illusionww <illusionww@gmail.com>
2014-04-28 17:22:53 +0400

    partically works

--- MPI_labN1/main.c
+++ MPI_labN1/main.c
@@ -1,23 +1,18 @@
 #include <stdio.h>
-#include <mpi.h>
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
 
-#define T 1
-#define X 1
 #define tau 0.01
-#define h 0.02
+#define h   0.05
 
-#define nT floor((double)T/(double)tau)
-#define nX floor((double)X/(double)h)
-#define PHI sin(x/300)
-#define PSI t*t
-#define F cos(t*x)
+#define nT floor(1.0/tau)
+#define nX floor(1.0/h)
 
-double phi(int k);
-double psi(int m);
-double f(int k, int m);
+#define PHI 1.0 - (double)k*tau     // it's k, [t][0]
+#define PSI 1.0 - (double)m*h // it's m, [0][x]
+#define F   1.0
+
 void err(char methodName[]);
 void showAll();
 void write();
@@ -25,22 +20,24 @@ void write();
 double **u;
 
 int main(int argc, char* argv[]) {
-    int k, m;
+    int k; // it's T
+    int m; // it's X
 
-    if ((double)tau/(double)h > 1.0) {
+    if (tau/h > 1.0) {
         printf("warning: unstable scheme\n");
     }
     
+    // allocate memory
     u = (double **) malloc((nT+1) * sizeof (double *));
     for (k = 0; k < (nT+1); k++)
         u[k] = (double *) malloc((nX+1) * sizeof (double));
 
     for (k = 0; k <= nT; k++) {
-        u[k][0] = phi(k);
+        u[k][0] = PHI;
     }
 
     for (m = 0; m <= nX; m++) {
-        u[0][m] = psi(m);
+        u[0][m] = PSI;
     }
 
     for (m = 1; m <= nX; m++) {
@@ -49,41 +46,13 @@ int main(int argc, char* argv[]) {
 
     for (k = 1; k < nT; k++) {
         for (m = 1; m < nX; m++) {
-            u[k+1][m] = 2*tau*(f(k, m) - (u[k][m+1] - u[k][m-1])/2/h) + u[k-1][m];
+            u[k+1][m] = u[k-1][m] - tau/h*(u[k][m+1] - u[k][m-1]) + 2*tau*F;
         }
     }
 
     write();
     
     return 0;
-}
-
-double phi(int k) {
-    double x = (double) k*h;
-
- //   if (x >= 0 && x <= X) {
-        return PHI;
- //   }
-    err("phi");
-}
-
-double psi(int m) {
-    double t = (double) m*h;
-
-//    if (t >= 0 && t <= T) {
-        return PSI;
-  //  }
-    err("psi");
-}
-
-double f(int k, int m) {
-    double x = (double) k*h;
-    double t = (double) m*h;
-
-//    if (x >= 0 && x <= X && t >= 0 && t <= T) {
-        return F;
- //   }
-    err("function");
 }
 
 void err(char methodName[]) {
@@ -97,7 +66,7 @@ void showAll() {
     printf("\n");
     for (k = nT; k >= 0; k--) {
         for (m = 0; m <= nX; m++) {
-            printf("%f\t", u[k][m]);
+            printf("%f ", u[k][m]);
         }
         printf("\n");
     }
@@ -108,32 +77,32 @@ void write() {
     int k, m;
     FILE *fp;
     
-    for (k = 0; k <= nT; k++) {
-        char name[80] = "";
-        char k_string[15];
+    char name[] = "output.txt";
+
+    fp = fopen(name, "wb");
+    printf("name: %s\n", name);
+    if(fp == NULL) {
+        printf("govno");
+        return;
+    }
         
-        strcpy(name,"out1/output");        
-        sprintf(k_string, "%d", k);
-        strcat(name, k_string);
-        strcat(name,".txt");
-        fp = fopen(name, "wb");
-        printf("name: %s\n", name);
-        if(fp == NULL) {
-            printf("govno");
-            return 0;
-	}
+    for (k = 0; k <= nT; k++) {
         for (m = 0; m <= nX; m++) {
             char str[80];
             char u_string[15];
             
-            sprintf(u_string, "%f", (double)m*h);
+            sprintf(u_string, "%f", (double)k*tau);
             strcpy(str, u_string);
+            strcat(str,"\t");
+            sprintf(u_string, "%f", (double)m*h);
+            strcat(str, u_string);
             strcat(str,"\t");
             sprintf(u_string, "%f", u[k][m]);
             strcat(str, u_string);
             strcat(str,"\n");
             fwrite(str, strlen(str), 1, fp);
         }
-        fclose(fp);
     }
+    
+    fclose(fp);
 }
