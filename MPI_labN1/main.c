39af3211a1e06532877b33b1c72a922da5892982
illusionww <illusionww@gmail.com>
2014-04-29 13:26:20 +0400

    works! / gnuplot scripts

--- MPI_labN1/main.c
+++ MPI_labN1/main.c
@@ -3,14 +3,14 @@
 #include <math.h>
 #include <string.h>
 
-#define tau 0.01
-#define h   0.05
+#define tau 0.001
+#define h   0.005
 
 #define nT floor(1.0/tau)
 #define nX floor(1.0/h)
 
-#define PHI 1.0 - (double)k*tau     // it's k, [t][0]
-#define PSI 1.0 - (double)m*h // it's m, [0][x]
+#define PHI 1.0 - (double)k*tau // it's k, [t][0]
+#define PSI 1.0 - (double)m*h   // it's m, [0][x]
 #define F   1.0
 
 void err(char methodName[]);
@@ -23,31 +23,28 @@ int main(int argc, char* argv[]) {
     int k; // it's T
     int m; // it's X
 
-    if (tau/h > 1.0) {
+    if (tau/h > 1.0)
         printf("warning: unstable scheme\n");
-    }
     
     // allocate memory
     u = (double **) malloc((nT+1) * sizeof (double *));
     for (k = 0; k < (nT+1); k++)
         u[k] = (double *) malloc((nX+1) * sizeof (double));
 
-    for (k = 0; k <= nT; k++) {
+    for (k = 0; k <= nT; k++)
         u[k][0] = PHI;
-    }
 
-    for (m = 0; m <= nX; m++) {
+    for (m = 0; m <= nX; m++)
         u[0][m] = PSI;
-    }
 
-    for (m = 1; m <= nX; m++) {
+    for (m = 1; m <= nX; m++)
         u[1][m] = u[0][m] + tau * u[0][m] + tau*tau/2*(u[0][m+1]-u[0][m-1])/2/h;
-    }
 
     for (k = 1; k < nT; k++) {
-        for (m = 1; m < nX; m++) {
+        for (m = 1; m < nX; m++)
             u[k+1][m] = u[k-1][m] - tau/h*(u[k][m+1] - u[k][m-1]) + 2*tau*F;
-        }
+        m = nX;
+        u[k+1][m] = u[k][m] - tau/h*(u[k][m] - u[k][m-1]) + tau*F;
     }
 
     write();
@@ -77,8 +74,7 @@ void write() {
     int k, m;
     FILE *fp;
     
-    char name[] = "output.txt";
-
+    char name[80] = "output.txt";
     fp = fopen(name, "wb");
     printf("name: %s\n", name);
     if(fp == NULL) {
@@ -87,22 +83,16 @@ void write() {
     }
         
     for (k = 0; k <= nT; k++) {
+        char p_string[15] = "";
+        sprintf(p_string, "%d %d\n", k, (int)nX+1);
+        fwrite(p_string, strlen(p_string), 1, fp);
         for (m = 0; m <= nX; m++) {
-            char str[80];
-            char u_string[15];
-            
-            sprintf(u_string, "%f", (double)k*tau);
-            strcpy(str, u_string);
-            strcat(str,"\t");
-            sprintf(u_string, "%f", (double)m*h);
-            strcat(str, u_string);
-            strcat(str,"\t");
-            sprintf(u_string, "%f", u[k][m]);
-            strcat(str, u_string);
-            strcat(str,"\n");
+            char str[80] = "";
+            sprintf(str, "%d %f %f\n", m+1, (double)m*h, u[k][m]);
             fwrite(str, strlen(str), 1, fp);
         }
+        fwrite("\n", strlen("\n"), 1, fp);
+        fwrite("\n", strlen("\n"), 1, fp);
     }
-    
     fclose(fp);
 }
