2b6f8e9f70329f297e5c069aa38d6814c4c53e53
illusionww <illusionww@gmail.com>
2014-04-29 13:48:41 +0400

    added "sum" - hello world on MPI

--- MPI_labN1/main.c
+++ MPI_labN1/main.c
@@ -13,8 +13,6 @@
 #define PSI 1.0 - (double)m*h   // it's m, [0][x]
 #define F   1.0
 
-void err(char methodName[]);
-void showAll();
 void write();
 
 double **u;
@@ -31,15 +29,17 @@ int main(int argc, char* argv[]) {
     for (k = 0; k < (nT+1); k++)
         u[k] = (double *) malloc((nX+1) * sizeof (double));
 
+    // initial / boundary conditions
     for (k = 0; k <= nT; k++)
         u[k][0] = PHI;
-
     for (m = 0; m <= nX; m++)
         u[0][m] = PSI;
 
+    // second layer
     for (m = 1; m <= nX; m++)
         u[1][m] = u[0][m] + tau * u[0][m] + tau*tau/2*(u[0][m+1]-u[0][m-1])/2/h;
 
+    // main cycle
     for (k = 1; k < nT; k++) {
         for (m = 1; m < nX; m++)
             u[k+1][m] = u[k-1][m] - tau/h*(u[k][m+1] - u[k][m-1]) + 2*tau*F;
@@ -52,47 +52,30 @@ int main(int argc, char* argv[]) {
     return 0;
 }
 
-void err(char methodName[]) {
-    printf("error in %s\n", methodName);
-    exit(1);
-}
-
-void showAll() {
-    int k, m;
-
-    printf("\n");
-    for (k = nT; k >= 0; k--) {
-        for (m = 0; m <= nX; m++) {
-            printf("%f ", u[k][m]);
-        }
-        printf("\n");
-    }
-    printf("\n");
-}
-
 void write() {
-    int k, m;
-    FILE *fp;
-    
     char name[80] = "output.txt";
-    fp = fopen(name, "wb");
-    printf("name: %s\n", name);
+    FILE *fp = fopen(name, "wb");
     if(fp == NULL) {
         printf("govno");
         return;
     }
-        
+    
+    int k;
     for (k = 0; k <= nT; k++) {
         char p_string[15] = "";
         sprintf(p_string, "%d %d\n", k, (int)nX+1);
         fwrite(p_string, strlen(p_string), 1, fp);
+        
+        int m;
         for (m = 0; m <= nX; m++) {
             char str[80] = "";
             sprintf(str, "%d %f %f\n", m+1, (double)m*h, u[k][m]);
             fwrite(str, strlen(str), 1, fp);
         }
-        fwrite("\n", strlen("\n"), 1, fp);
-        fwrite("\n", strlen("\n"), 1, fp);
+        if (k < nT) {
+            fwrite("\n", strlen("\n"), 1, fp);
+            fwrite("\n", strlen("\n"), 1, fp);
+        }
     }
     fclose(fp);
 }
---
+++ sum/sum.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+#include<mpi.h>
+#include<stdlib.h>
+
+int main(int argc, char* argv[]) {
+    int n;
+    int rank, coreNum, type = 2;
+    MPI_Status status;
+    
+    n = atoi(argv[1]);
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &coreNum);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    if (rank == 0) {
+        double sum = 0;
+        int frame = n / coreNum;
+        int i;
+
+        printf("core count = %d\n", coreNum);
+
+	for (i = 1; i <= frame; i++) {
+	    sum += 1.0/i;
+	}	
+
+        for (i = 1; i < coreNum; i++) {
+            int left = i* frame;
+            int right = (i+1)*frame > n ? n : (i+1)*frame;
+
+            MPI_Send(&left, 1, MPI_INT, i, type, MPI_COMM_WORLD);
+            MPI_Send(&right, 1, MPI_INT, i, type, MPI_COMM_WORLD);
+        }
+
+        for (i = 1; i < coreNum; i++) {
+            double middleSum;
+            MPI_Recv(&middleSum, 1, MPI_DOUBLE, i, type, MPI_COMM_WORLD, &status);
+            sum += middleSum;
+
+
+        }
+        printf("summa = %g\n", sum);
+    } else {
+        double sum = 0;
+        int left, right;
+        int i;
+
+        MPI_Recv(&left, 1, MPI_INT, 0, type, MPI_COMM_WORLD, &status);
+        MPI_Recv(&right, 1, MPI_INT, 0, type, MPI_COMM_WORLD, &status);
+
+        for (i = left + 1; i <= right; i++) {
+            if (i != 0) {
+                sum += 1.0 / i;
+            }
+        }
+
+        printf("%d: %d..%d\tsum = %g\n", rank, left, right, sum);
+
+        MPI_Send(&sum, 1, MPI_DOUBLE, 0, type, MPI_COMM_WORLD);
+    }
+
+    MPI_Finalize();
+}
