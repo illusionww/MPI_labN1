9419e752ec26f633c4b2056cd4c1953f3bf46f65
illusionww <illusionww@gmail.com>
2014-04-30 13:54:13 +0400

    fuck yeah it works!

--- MPI_labN1/main.c
+++ MPI_labN1/main.c
@@ -5,22 +5,23 @@
 #include <mpi.h>
 
 #define tau 0.001
-#define h   0.125
+#define h   0.005
 
 #define nT floor(1.0/tau)
 #define nX floor(1.0/h)
 
-#define SXLEN (int)floor(nX/coreNum)
-#define MXLEN (int)(nX - (coreNum - 1)*SXLEN)
+#define SXLEN floor(nX/coreNum)
+#define MXLEN (nX - (coreNum - 1)*SXLEN)
 
 #define PHI(k) 1.0 - (double)k*tau // it's k, [t][0]
 #define PSI(m) 1.0 - (double)m*h   // it's m, [0][x]
-#define F(k,m) 1.0
+#define F   1.0
 
 #define TYPE 2
 
 void masterThread();
 void slaveThread(int coreNum, int rank);
+void wr(double **u, double ***n, int coreNum);
 
 MPI_Status status;
 
@@ -36,6 +37,8 @@ int main(int argc, char* argv[]) {
             printf("warning: unstable scheme\n");
         printf("number of cores: %d\n", coreNum);
         printf("nX: %f\n", nX);
+        printf("MXLEN: %f\n", MXLEN);
+        printf("SXLEN: %f\n", SXLEN);
         masterThread(coreNum, rank); 
     } else {
         slaveThread(coreNum, rank);
@@ -44,16 +47,14 @@ int main(int argc, char* argv[]) {
     MPI_Finalize();
 }
 
-void masterThread(int coreNum, int rank) {
-    double **u;
-    
+void masterThread(int coreNum, int rank) {  
     int k; // it's T
     int m; // it's X
     
     // allocate memory
-    u = (double **) malloc((nT+1) * sizeof (double *));
+    double **u = (double **) malloc((nT+1) * sizeof (double *));
     for (k = 0; k < (nT+1); k++)
-        u[k] = (double *) malloc(MXLEN * sizeof (double));
+        u[k] = (double *) malloc((MXLEN+1) * sizeof (double));
     
     // initial&boundary conditions
     for (k = 0; k <= nT; k++)
@@ -64,60 +65,121 @@ void masterThread(int coreNum, int rank) {
     // second layer
     for (m = 1; m <= MXLEN; m++)
         u[1][m] = u[0][m] + tau * u[0][m] + tau*tau/2*(u[0][m+1]-u[0][m-1])/2/h;
+    if (coreNum > 1) {
+        MPI_Send(&(u[1][(int)MXLEN]), 1, MPI_DOUBLE, 1, TYPE, MPI_COMM_WORLD);
+    }
     
     // main cycle
     for (k = 1; k < nT; k++) {
         for (m = 1; m < MXLEN; m++)
-            u[k+1][m] = u[k-1][m] - tau/h*(u[k][m+1] - u[k][m-1]) + 2*tau*F(k, m);
-        u[k+1][m] = u[k][m] - tau/h*(u[k][m] - u[k][m-1]) + tau*F(k, MXLEN);
-        if (rank+1 != coreNum) 
-            MPI_Send(&u[k+1][m], 1, MPI_DOUBLE, rank+1, TYPE, MPI_COMM_WORLD);
+            u[k+1][m] = u[k-1][m] - tau/h*(u[k][m+1] - u[k][m-1]) + 2*tau*F;
+        m = (int)MXLEN;
+        u[k+1][m] = u[k][m] - tau/h*(u[k][m] - u[k][m-1]) + tau*F;
+        if (rank+1 != coreNum) {
+            MPI_Send(&(u[k+1][m]), 1, MPI_DOUBLE, rank+1, TYPE, MPI_COMM_WORLD);
+        }
     }
     
     int i;
     double ***n = (double ***) malloc((coreNum-1) * sizeof (double **));
-    for (i = 1; i < coreNum; i++){
+    for (i = 0; i < coreNum-1; i++){
         n[i] = (double **) malloc((nT+1) * sizeof (double *));
         for (k = 0; k < (nT+1); k++) {
             n[i][k] = (double *) malloc(SXLEN * sizeof (double));
         }
     }
     
-    for (i = 1; i < coreNum; i++) {
-        MPI_Recv(&n[i][0][0], (nT+1)*(SXLEN+1), MPI_DOUBLE, i, TYPE, MPI_COMM_WORLD, &status);
+    for (i = 0; i < coreNum-1; i++) {
+        for (k = 0; k < nT; k++) {
+            MPI_Recv(&(n[i][k][0]), (SXLEN+1), MPI_DOUBLE, i+1, TYPE, MPI_COMM_WORLD, &status);
+        }
     }
     
+    wr(u, n, coreNum);
 }
 
 void slaveThread(int coreNum, int rank) {
-    double **u;
-    
     int k; // it's T
     int m; // it's X
     
     // allocate memory
-    u = (double **) malloc((nT+1) * sizeof (double *));
+    double **u = (double **) malloc((nT+1) * sizeof (double *));
     for (k = 0; k < (nT+1); k++)
         u[k] = (double *) malloc((SXLEN+1) * sizeof (double));
     
     // initial conditions
     for (m = 0; m <= SXLEN; m++)
-        u[0][m] = PSI(m + nX - (coreNum - rank)*floor(nX/coreNum));
+        u[0][m] = PSI((int)(MXLEN + SXLEN*(rank-1) + m));
     
     // second layer
-    for (m = 0; m <= SXLEN; m++)
+    for (m = 1; m <= SXLEN; m++)
         u[1][m] = u[0][m] + tau * u[0][m] + tau*tau/2*(u[0][m+1]-u[0][m-1])/2/h;
+    if (rank+1 != coreNum)
+        MPI_Send(&(u[1][(int)SXLEN]), 1, MPI_DOUBLE, rank+1, TYPE, MPI_COMM_WORLD);
     
     // main cycle
     for (k = 1; k < nT; k++) {
-        MPI_Recv(&u[k+1][0], 1, MPI_DOUBLE, rank-1, TYPE, MPI_COMM_WORLD, &status);
+        MPI_Recv(&(u[k][0]), 1, MPI_DOUBLE, rank-1, TYPE, MPI_COMM_WORLD, &status);
         
         for (m = 1; m < SXLEN; m++)
-            u[k+1][m] = u[k-1][m] - tau/h*(u[k][m+1] - u[k][m-1]) + 2*tau*F(k, m);
-        u[k+1][m] = u[k][m] - tau/h*(u[k][m] - u[k][m-1]) + tau*F(k, SXLEN);
+            u[k+1][m] = u[k-1][m] - tau/h*(u[k][m+1] - u[k][m-1]) + 2*tau*F;
+        m = (int)SXLEN;
+        u[k+1][m] = u[k][m] - tau/h*(u[k][m] - u[k][m-1]) + tau*F;
         if (rank+1 != coreNum) 
-            MPI_Send(&u[k+1][m], 1, MPI_DOUBLE, rank+1, TYPE, MPI_COMM_WORLD);
+            MPI_Send(&(u[k+1][m]), 1, MPI_DOUBLE, rank+1, TYPE, MPI_COMM_WORLD);
     }
     
-    MPI_Send(&u[0][0], (nT+1)*(SXLEN+1), MPI_DOUBLE, 0, TYPE, MPI_COMM_WORLD);
+    for (k = 0; k < nT; k++) {
+        MPI_Send(&(u[k][0]), (SXLEN+1), MPI_DOUBLE, 0, TYPE, MPI_COMM_WORLD);
+    }
+    
+    char str[15];
+    sprintf(str, "%d", rank);
+    FILE *fp = fopen(str, "wb");
+    for (k = 0; k <= nT; k++) {
+        for (m = 0; m <= SXLEN; m++) {
+            char str[80] = "";
+            sprintf(str, "%d %d %f\n", k, m, u[k][m]);
+            fwrite(str, strlen(str), 1, fp);
+        }
+    }
+    fclose(fp);
+}
+
+void wr(double **u, double ***n, int coreNum) {
+    FILE *fp = fopen("output.txt", "wb");
+    if(fp == NULL) {
+        printf("govno");
+        return;
+    }
+    
+    int k;
+    for (k = 0; k <= nT; k++) {
+        char p_string[15] = "";
+        sprintf(p_string, "%d %d\n", k, (int)nX+1);
+        fwrite(p_string, strlen(p_string), 1, fp);
+        
+        int m;
+        for (m = 0; m <= MXLEN; m++) {
+            char str[80] = "";
+            sprintf(str, "%d %f %f\n", m+1, m*h, u[k][m]);
+            fwrite(str, strlen(str), 1, fp);
+        }
+        
+        int i;
+        for (i = 0; i < coreNum-1; i++) {
+            for (m = 1; m <= SXLEN; m++) {
+                char str[80] = "";
+                int t = (int)MXLEN + SXLEN*i + m;
+                sprintf(str, "%d %f %f\n", t+1, t*h, n[i][k][m]);
+                fwrite(str, strlen(str), 1, fp);
+            }
+        }
+        
+        if (k < nT) {
+            fwrite("\n", strlen("\n"), 1, fp);
+            fwrite("\n", strlen("\n"), 1, fp);
+        }
+    }
+    fclose(fp);
 }
